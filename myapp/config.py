7be783c15c166da759f158bbf165e2da4c1585dd
lelele <quangle2072008@gmail.com>
2024-08-29 15:50:19 +0700

    Initial commit

--- myapp/config.py
+++ myapp/config.py
@@ -4,12 +4,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy.sql import func
-app = Flask(__name__,template_folder="frontend")
+class Config:
+    """Base configuration."""
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'lelethecoder')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:lele9920483@localhost:5432/model')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Create the Flask app
+app = Flask(__name__, template_folder="frontend")
+app.config.from_object(Config)
+
+# Initialize CORS
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lele9920483@localhost:5432/model'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#postgresql://leledatabase_user:gllglkTmoT3sZmkWp1HzboT2AsYXeW5a@dpg-cr811g3tq21c739hlq40-a.oregon-postgres.render.com/leledatabase
-
-
+# Initialize SQLAlchemy
 db = SQLAlchemy(app)
