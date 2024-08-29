0beb2a37af53d42ffcb1797b2f541a68de431fa0
lelele <quangle2072008@gmail.com>
2024-08-29 16:37:57 +0700

    Initial commit

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from myapp.config import app
+from myapp import app
 from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Set debug to False in production
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
