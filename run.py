b31b347004f90df31b80d9cde73e1fac559da7f8
lelele <quangle2072008@gmail.com>
2024-08-29 16:46:31 +0700

    Initial commit

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 from myapp.config import app
-from myapp import app
+from myapp import app,create_app
 from flask import Flask
 app = Flask(__name__)
-
+app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
