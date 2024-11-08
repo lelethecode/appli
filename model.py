14250ca22d2dcac7168a1f135e617c9f0b472f20
lelele <quangle2072008@gmail.com>
2024-11-08 20:29:11 +0700

    ok

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ class Contact(db.Model):
     __tablename__ = 'contact' 
     id = db.Column(db.Integer, primary_key=True)
     check = db.Column(db.Integer)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     clas = db.Column(db.String(80), unique=False, nullable=False)
     password = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
