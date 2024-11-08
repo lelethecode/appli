94c67d44b99c03dd772b36f5c11bc6b4af0b6d88
lelele <quangle2072008@gmail.com>
2024-11-08 21:04:07 +0700

    ok

--- up.py
+++
@@ -1,7 +0,0 @@
-from main import db
-from model import Contact
-
-# Example: Update all users to set `username` as not unique (if it was altered)
-# or any other field updates you need.
-Contact.query.update({Contact.username: 'new_username_value'})
-db.session.commit()
